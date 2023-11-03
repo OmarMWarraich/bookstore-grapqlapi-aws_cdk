ef22763625be4d4e5894d0c8252fcef95e4c6245
OmarMWarraich <earthling986@gmail.com>
2023-11-03 12:27:01 +0500

    feat: AppSync Schema Default Authorization

--- lib/book-store-graphql-api-stack.ts
+++ lib/book-store-graphql-api-stack.ts
@@ -1,11 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as appsync from "aws-cdk-lib/aws-appsync";
+import * as path from 'path';
 
 export class BookStoreGraphqlApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const hello: string = 'Hello, CDK!';
+    const api = new appsync.GraphqlApi(this, "BookStoreApi", {
+      name: "book-store-api",
+      definition: appsync.Definition.fromFile(path.join(__dirname, '../graphql/schema.graphql')),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            name: "BookStoreApiKey",
+            expires: cdk.Expiration.after(cdk.Duration.days(365))
+          }
+        },
+      }
+    });
   }
 }
